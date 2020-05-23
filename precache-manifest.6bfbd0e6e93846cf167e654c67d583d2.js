6c6d9f4feb033c420da2d3bc6df08f575fd2df61
Jared <jemraider21@gmail.com>
2020-05-22 20:51:23 -0400

    Updates

---
+++ precache-manifest.6bfbd0e6e93846cf167e654c67d583d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611f975a98356b4ec25ef8b088430ff4",
+    "url": "/react-face-recognition/index.html"
+  },
+  {
+    "revision": "55bfd520a48b90a3fbe3",
+    "url": "/react-face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7761eed8e87f35711067",
+    "url": "/react-face-recognition/static/css/main.d4b1b488.chunk.css"
+  },
+  {
+    "revision": "55bfd520a48b90a3fbe3",
+    "url": "/react-face-recognition/static/js/2.539b1d6f.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/react-face-recognition/static/js/2.539b1d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7761eed8e87f35711067",
+    "url": "/react-face-recognition/static/js/main.174c06e0.chunk.js"
+  },
+  {
+    "revision": "ce1fdfbb7952e0e6e3f9",
+    "url": "/react-face-recognition/static/js/runtime-main.3add8129.js"
+  }
+]);
